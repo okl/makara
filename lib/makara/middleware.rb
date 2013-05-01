7a2a9a75de38254e4b6b3514848b27b3876c33a2
matt camuto <mattcamuto@onekingslane.com>
2013-05-01 13:44:30 -0700

    added logging

--- lib/makara/middleware.rb
+++ lib/makara/middleware.rb
@@ -12,12 +12,12 @@ module Makara
       @request = Rack::Request.new(env)
 
       status, headers, body = if should_force_database?
-        Makara.connection.with_master do
-          @app.call(env)
-        end
-      else
-        @app.call(env)
-      end
+                                Makara.connection.with_master do
+                                  @app.call(env)
+                                end
+                              else
+                                @app.call(env)
+                              end
 
       @response = Rack::Response.new(body, status, headers)
 
@@ -37,18 +37,25 @@ module Makara
 
     # currently just use master. flexibility coming soon.
     def database_to_force
+      Makara.info("Database to force request")
       @request.cookies['makara-force-master']
     end
 
     def store_master_cookie!
       if @request.get?
-        return if [301, 302].include?(@response.status.to_i)
+        if [301, 302].include?(@response.status.to_i)
+          Makara.info ("Redirect, skipping cookie delete")
+          return
+        end
 
-        if @response.header['Set-Cookie'].present? 
+        if @response.header['Set-Cookie'].present?
+          Makara.info ("Deleting force master cookie")
           @response.delete_cookie('makara-force-master')
         end
       elsif Makara.connection.sticky_master? && Makara.connection.currently_master?
         ttl = Time.at(Time.now.to_i + 5)
+        Makara.info ("Setting makara cookie value to #{ Makara.connection.current_wrapper_name}")
+
         @response.set_cookie('makara-force-master', {:value => Makara.connection.current_wrapper_name, :expires => ttl})
       end
     end
